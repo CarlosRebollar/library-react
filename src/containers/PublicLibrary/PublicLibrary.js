98d8952af0eb996417badba94fd2167b2cbebf7c
Carlos Palacios Rebollar <carlos.palaciosrebollar@gmail.com>
2018-06-09 17:42:23 -0500

    My fixed second commit

--- src/containers/PublicLibrary/PublicLibrary.js
+++ src/containers/PublicLibrary/PublicLibrary.js
@@ -14,9 +14,9 @@ class PublicLibrary extends Component {
         return (
             <Grid>
                 <Switch>
-                    <Route path='library-react/catalog' component={Catalog} />
-                    <Route path='library-react/contact' component={Contact} />
-                    <Route path='library-react/' component={Home}/>
+                    <Route path='/library-react/catalog' component={Catalog} />
+                    <Route path='/library-react/contact' component={Contact} />
+                    <Route path='/library-react/' component={Home}/>
                 </Switch>
             </Grid>
         );
