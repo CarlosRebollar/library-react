f22dd527dd34bceda549d3020a12a24d5d47cc3c
Carlos Palacios Rebollar <carlos.palaciosrebollar@gmail.com>
2018-06-09 17:40:49 -0500

    My fixed commit

--- src/containers/Library/Library.js
+++ src/containers/Library/Library.js
@@ -6,9 +6,9 @@ import NavbarItems from '../../components/Navigation/NavigationItems/navigationI
 class Library extends Component {
     state = {
         navItems: [
-            { link: '/', text: 'Home' },
-            { link: '/catalog', text: 'Catalog' },
-            { link: '/contact', text: 'Contact' }
+            { link: 'library-react/', text: 'Home' },
+            { link: 'library-react/catalog', text: 'Catalog' },
+            { link: 'library-react/contact', text: 'Contact' }
         ]
     }
     render() {
